30c5aeff72ccc7ab1556cc9a0891f685e1a4ce74
Aleksandar Mitic <aleksandar.webart@gmail.com>
2018-08-03 14:04:35 +0200

    Isolating the Layout

--- flatphp/templates/post_list.php
+++ flatphp/templates/post_list.php
@@ -1,18 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>List of Posts</title>
-    </head>
-    <body>
-        <h1>List of Posts</h1>
-        <ul>
-            <?php foreach ($posts as $post): ?>
-            <li>
-                <a href="<?php echo $_SERVER['REQUEST_URI'] ?>show.php?id=<?= $post['id'] ?>">
-                    <?= $post['post_title'] ?>
-                </a>
-            </li>
-            <?php endforeach ?>
-        </ul>
-    </body>
-</html>
+<?php $title = 'List of Posts' ?>
+
+<?php ob_start() ?>
+    <h1>List of Posts</h1>
+    <ul>
+        <?php foreach ($posts as $post): ?>
+        <li>
+            <a href="<?php echo $_SERVER['REQUEST_URI'] ?>/show.php?id=<?= $post['id'] ?>">
+                <?= $post['post_title'] ?>
+            </a>
+        </li>
+        <?php endforeach ?>
+    </ul>
+<?php $content = ob_get_clean() ?>
+
+<?php include 'layout.php' ?>
